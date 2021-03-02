f1fba9ba3d187a63fc36c969dd8d1b51e1fb79a8
laurapoc <laura.pociuniene@gmail.com>
2021-03-02 19:05:07 +0200

    add image slide to the homepage (MainContainer.js)

--- src/components/imageSlide/ImageSlide.js
+++ src/components/imageSlide/ImageSlide.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 import ImageGallery from "react-image-gallery";
 import "../../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 import classes from "./ImageSlide.module.css";
 
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
-
 export default class ImageSlide extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://vitrazai.lt/wp-json/wp/v2/media").then((res) => {
+      const posts = res.data;
+      this.setState({ posts: posts });
+    });
+  }
+
   render() {
+    // create image slide source object array from fetched data:
+    let imageSources = this.state.posts.map((post) => {
+      return { original: post.source_url };
+    });
+
     return (
       <div className={classes.Imageslide}>
-        <ImageGallery items={images} autoPlay={true} showPlayButton={true} showThumbnails={false} showFullscreenButton={false} additionalClass="PlayButton"/>;
+        <ImageGallery
+          items={imageSources}
+          autoPlay={true}
+          showPlayButton={true}
+          showThumbnails={false}
+          showFullscreenButton={false}
+          slideDuration={900}
+          showNav={false}
+        />
+        ;
       </div>
     );
   }

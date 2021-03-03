99b354ad1f551eab66593c3112044305347dc204
Laura P <laura.pociuniene@gmail.com>
2021-03-03 12:10:59 +0200

    fix slide's styling

--- src/components/imageSlide/ImageSlide.js
+++ src/components/imageSlide/ImageSlide.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import ImageGallery from "react-image-gallery";
 import "../../../node_modules/react-image-gallery/styles/css/image-gallery.css";
-import classes from "./ImageSlide.module.css";
+import "./ImageSlide.css"
+// import classes from "./ImageSlide.module.css";
 
 export default class ImageSlide extends Component {
   state = {
@@ -17,21 +18,28 @@ export default class ImageSlide extends Component {
   }
 
   render() {
+    console.log(this.state.posts);
     // create image slide source object array from fetched data:
     let imageSources = this.state.posts.map((post) => {
-      return { original: post.source_url };
+      console.log(post.media_details.sizes.thumbnail.source_url);
+      return {
+        original: post.source_url,
+        thumbnail: post.media_details.sizes.thumbnail.source_url,
+       
+      };
     });
 
     return (
-      <div className={classes.Imageslide}>
+      <div className="Imageslide">
         <ImageGallery
           items={imageSources}
           autoPlay={true}
           showPlayButton={true}
-          showThumbnails={false}
+          showThumbnails={true}
           showFullscreenButton={false}
           slideDuration={900}
           showNav={false}
+    
         />
         ;
       </div>

edf57b219de41633da57b746a57da7b980dc4082
laurapoc <laura.pociuniene@gmail.com>
2021-03-27 10:02:17 +0200

    commit before creating new branch

--- src/components/posts/testGallery/galleryImage/GalleryImage.js
+++ src/components/posts/testGallery/galleryImage/GalleryImage.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 export default class GalleryImage extends Component {
   render() {
-    const { title, imageSource } = this.props;
+    const { title, imageSource, alt } = this.props;
     return (
       <div>
         <h3>{title}</h3>
-        <img src={imageSource} alt="vitrazas" />
+        <img src={imageSource} alt={alt} />
       </div>
     );
   }
--- src/components/posts/testGallery/galleryImages/GalleryImageList.js
+++ src/components/posts/testGallery/galleryImages/GalleryImageList.js
@@ -7,10 +7,11 @@ export default class GalleryImageList extends Component {
     gallery: [],
   };
 
+  // fetch posts from new WP acount, working yet on localhost:
   async componentDidMount() {
     try {
       let response = await axios.get("http://localhost/vitrazai/wp-json/wp/v2/gallery");
-        console.log(response.data);
+      console.log(response.data);
       if (!response.data) {
         throw new Error("Something went wrong...");
       }
@@ -22,7 +23,14 @@ export default class GalleryImageList extends Component {
 
   render() {
     console.log(this.state.gallery);
-    const galleryItemList = this.state.gallery.map((item) => <GalleryImage key={item.id} title={item.title.rendered} imageSource={item.acf.image.sizes.medium} />);
+    const galleryItemList = this.state.gallery.map((item) => (
+      <GalleryImage
+        key={item.id}
+        title={item.title.rendered}
+        imageSource={item.acf.image.sizes.medium}
+        alt={item.acf.image.alt}
+      />
+    ));
     return (
       <div>
         <h2>TESTINĖ GALERIJA IŠ NAUJO WORDPRESSO</h2>
